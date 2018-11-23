9232446f8f3a99ed01918002a6c16bdb220436e5
pjainan <jainani.pankaj@gmail.com>
2018-11-23 22:43:46 +0530

    Captured images as array into pickle

--- AV01-ITA/dataprocessing.py
+++ AV01-ITA/dataprocessing.py
@@ -1,33 +1,53 @@
 import numpy as np
 import io
+import os
 import os.path as ops
 import pickle
 import pandas as pd
+# import matplotlib.pyplot as plt
+from skimage.io import imread
 
 class DataHelper:
     __datafolder = "./data/"
     __testfolder = "test"
     __trainfolder = "train"
-    __trainfile = "train.csv"
-    __trainfile = "test.csv"
+    __trainfile = "train"
+    __trainfile = "test"
+    __file_ext = ".csv"
 
     __pickle_obj = {}
+    __unpickle_obj = {}
 
     def __init__(self):
         pass
    
-    def __pickle_dump(self,pobj):
+    def pickle_dump(self):
         picklefileobj = open(ops.abspath(self.__datafolder+"proj.pickle"), 'ab')
-        pickle.dump(pobj, picklefileobj)
+        pickle.dump(self.__pickle_obj, picklefileobj)
+        picklefileobj.close()
+
+    def pickle_extract(self):
+        picklefileobj = open(ops.abspath(self.__datafolder+"proj.pickle"), 'rb')
+        __unpickle_obj = pickle.load(picklefileobj)
+        for keys in __unpickle_obj:
+            print(keys)
         picklefileobj.close()
 
     def __read_data(self, mode, path):
         rd = pd.read_csv(path)
         self.__pickle_obj[mode] = rd
-        self.__pickle_dump(self.__pickle_obj)
         return rd
 
-    def read_train_data(self):
-        rd_train = (self.__read_data('train',ops.abspath(self.__datafolder+self.__trainfile)))
-        print(rd_train.head())
-        return rd_train
+    def get_data(self, mode):
+        data = self.__read_data(mode,ops.abspath(self.__datafolder+mode+self.__file_ext))
+        return data
+
+    def fill_arrays(self, mode):
+        imgs = []
+        image_folder = ops.abspath(self.__datafolder+mode+"/")
+        for dirpath, dirs, images in os.walk(image_folder):
+            for img in images:
+                imgs.append(imread(ops.join(image_folder, img)))
+            print("captured images data : " + mode)
+            self.__pickle_obj[mode+"_images"] = imgs
+                            
--- AV01-ITA/hack.py
+++ AV01-ITA/hack.py
@@ -1,4 +1,13 @@
 import dataprocessing as dp
 
-dh = dp.DataHelper()
-dh.read_train_data()
+def ProcessData():
+    dh = dp.DataHelper()
+    dh.get_data('train')
+    dh.get_data('test')
+    dh.fill_arrays('test')
+    dh.fill_arrays('train')
+    dh.pickle_dump()
+    dh.pickle_extract()
+
+if __name__ == '__main__':
+    ProcessData()
